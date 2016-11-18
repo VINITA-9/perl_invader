d90fd54d608d121640cbf2645c4c5aa373f3ae1c
VINITA-9 <13bit058@nirmauni.ac.in>
2016-11-17 21:29:56 -0800

    Add files via upload

---
+++ Game-PerlInvaders-master/lib/Game/PerlInvaders.pm
@@ -0,0 +1,42 @@
+package Game::PerlInvaders;
+use 5.010;
+use strict;
+use warnings;
+
+use version 0.77; our $VERSION = qv('0.0.1');
+1;
+
+__END__
+
+=head1 NAME
+
+Game::PerlInvaders - space invaders game implemented using SDL
+
+=head1 SYNOPSIS
+
+  # no usage atm
+
+=head1 DESCRIPTION
+
+A rather simple game of Space Invaders, implemented in a few hours in my Unix/Linux machine to get people interested in developing games 
+using Perl and SDL.
+
+Installation currently does not work, so you should run PerlInvaders 
+in the project directory:
+  
+  perl -Ilib bin/perl_invaders.pl
+
+=head1 AUTHOR
+
+Bruno Ramalhete
+mail: <bram.512@gmail.com>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
